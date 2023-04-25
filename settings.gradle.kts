413b57a1f39cec4714fe390a928f0d39de0ab080
GitHub Action <action@github.com>
2023-04-25 08:19:30 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "embrace-intellij-platform-plugin"
--- src/main/kotlin/com/github/gnerv/embraceintellijplatformplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gnerv/embraceintellijplatformplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.gnerv.embraceintellijplatformplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gnerv.embraceintellijplatformplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
